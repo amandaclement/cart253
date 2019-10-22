e912e680a24cf66358535d73419e945f1dcb79ec
Amanda Clement <amanda.clement22@hotmail.com>
2019-10-22 16:49:58 -0400

    Ex6: Fixed issue of movement pattern of Prey - made noise range from (0,1000) instead of (0,0)

--- exercises/exercise6/js/Prey.js
+++ exercises/exercise6/js/Prey.js
@@ -23,8 +23,9 @@ class Prey { ////////////// FIXED
     this.vy = 0;
     this.speed = speed; ////////////// FIXED
     // Time properties for noise() function
-    this.tx = random(0, 0); // To make x and y noise different
-    this.ty = random(0, 0); // we use random starting values
+    ///////// FIXED
+    this.tx = random(0, 1000); // To make x and y noise different
+    this.ty = random(0, 1000); // we use random starting values
     // Health properties
     this.maxHealth = radius;
     this.health = this.maxHealth; // Must be AFTER defining this.maxHealth
